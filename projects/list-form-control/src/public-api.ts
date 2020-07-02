74d0c741e78b094840afeed1d8ff99117b3dc860
Stefani Nikolova <stefani.v.nikolova@gmail.com>
2020-07-02 13:38:46 +0300

    Remove service files

--- projects/list-form-control/src/public-api.ts
+++ projects/list-form-control/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of list-form-control
  */
 
-export * from './lib/list-form-control.service';
 export * from './lib/list-form-control.module';
 export * from './lib/directives/list-control.directive';
 export * from './lib/directives/focus-value-match.directive';
